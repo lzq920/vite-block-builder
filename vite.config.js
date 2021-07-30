dc71a0b59a30d8fcdb363fcfc1b82ba90fa53944
eleven <lizhiqiang@meb.com>
2021-07-30 11:27:38 +0800

    添加jsx支持

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import vue from '@vitejs/plugin-vue'
-import {defineConfig} from "vite"
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
-plugins:[vue()]
+  plugins: [vue(), vueJsx()]
 })
