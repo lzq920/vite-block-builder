e5dc84df5e27fddec7c8298c096d59223a4ef983
eleven <lzq920@vip.qq.com>
2021-08-27 21:47:37 +0800

    ✨ element-plus

--- src/components/main-layout/index.vue
+++ src/components/main-layout/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-container style="height: 500px; border: 1px solid #eee">
+  <el-container class="w-screen h-screen overflow-hidden">
     <el-aside
       width="200px"
       style="background-color: rgb(238, 241, 246)"
@@ -96,7 +96,7 @@
     </el-aside>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
+      <el-header>
         <el-dropdown>
           <i
             class="el-icon-setting"
@@ -114,7 +114,9 @@
       </el-header>
 
       <el-main>
-        <router-view />
+        <el-scrollbar>
+          <router-view />
+        </el-scrollbar>
       </el-main>
     </el-container>
   </el-container>
--- src/views/pages/index.vue
+++ src/views/pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>page list</div>
+  <el-select
+    v-model="data"
+    placeholder=""
+  >
+    <el-option
+      v-for="item in 100"
+      :key="item"
+    />
+  </el-select>
+  <div
+    v-for="item in 1000"
+    :key="item"
+  >
+    page list
+  </div>
 </template>
 
 <script>
